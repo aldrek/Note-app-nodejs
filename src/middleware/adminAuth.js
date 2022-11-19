1766f37d48d530a22917b1a6a99417f34fdc03c6
Ahmad salman <ahmadg.salman@hotmail.com>
2022-11-19 10:51:01 +0200

    format code

--- src/middleware/adminAuth.js
+++ src/middleware/adminAuth.js
@@ -1,13 +1,11 @@
-module.exports = (req , res , next) =>{
-
-    const user = req.user;
-    if(user.isAdmin !== true){
-        res.json({
-            status : "false" ,
-            message : "You are not allowed to this"
-        })
-    }else{
-        next()
-    }
- 
-}
+module.exports = (req, res, next) => {
+  const user = req.user;
+  if (user.isAdmin !== true) {
+    res.json({
+      status: "false",
+      message: "You are not allowed to this",
+    });
+  } else {
+    next();
+  }
+};
--- src/middleware/apiAuth.js
+++ src/middleware/apiAuth.js
@@ -1,13 +1,13 @@
-require('dotenv').config()
+require("dotenv").config();
 
 // Check if api key is the same as the one from server
-module.exports = (req , res , next) => {
-    const apiKey = req.headers.api_key
-    if(process.env.API_KEY !== apiKey){
-        res.send({
-            message : "You are not allowed to use this api"
-        })
-    }else{
-        next()
-    }
-}
+module.exports = (req, res, next) => {
+  const apiKey = req.headers.api_key;
+  if (process.env.API_KEY !== apiKey) {
+    res.send({
+      message: "You are not allowed to use this api",
+    });
+  } else {
+    next();
+  }
+};
